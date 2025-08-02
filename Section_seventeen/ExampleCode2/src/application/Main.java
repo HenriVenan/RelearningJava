9229ec3593c28bb8e3aaab8d362f67f50eb80240
Bino <abnerhbraga@outlook.pt>
2025-08-01 22:25:04 -0300

    ExampleCode2: v1

--- Section_seventeen/ExampleCode2/src/application/Main.java
+++ Section_seventeen/ExampleCode2/src/application/Main.java
@@ -1,10 +1,30 @@
 package application;
 
+import entities.Product;
+import services.CalculationService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-      out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
 
+        out.print("How many products will be added?: ");
+        int amountProducts = sc.nextInt();
+        List<Product> products = new ArrayList<>();
+
+        for (int i = 0; i < amountProducts; i++) {
+            out.printf("Product %dº: ", (i + 1));
+            String productAndPriceString = sc.next();
+            String[] productAndPrice = productAndPriceString.split(",");
+            products.add(new Product(productAndPrice[0], Double.parseDouble(productAndPrice[1])));
+        }
+
+        out.println("\nMost expensive: ");
+        out.println(CalculationService.max(products));
     }
 }
